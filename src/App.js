4e69e1f026ec115e14667961bd0bb57a95555067
Ayush Bafna <abafna@businessolver.com>
2023-03-27 14:56:48 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import "./App.css";
 import ReactHlsPlayer from "react-hls-player";
 
 function App() {
+  const playerRef = React.useRef();
+
   return (
     <div className="App">
       <ReactHlsPlayer
+        playerRef={playerRef}
         src="http://tirth.onrender.com/live/Tirth/index.m3u8"
         controls={true}
-        width="100%"
-        height="auto"
-        hlsConfig={{
-          maxLoadingDelay: 4,
-          minAutoBitrate: 0,
-          lowLatencyMode: true
-        }}
       />
     </div>
   );
